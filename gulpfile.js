28521d1089bda81646f39c03d2fa879f6ede796a
Ben Drucker <bvdrucker@gmail.com>
2014-02-27 11:07:41 -0500

    gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var gulp    = require('gulp');
+var gutil   = require('gulp-util');
+var plugins = require('gulp-load-plugins')();
+
+gulp.task('lint', function () {
+  return gulp.src(['src/**', 'test/**'])
+    .pipe(plugins.jshint())
+    .pipe(plugins.jshint.reporter('jshint-stylish'));
+});
+
+gulp.task('cover', function () {
+  return gulp.src('src/**')
+    .pipe(plugins.instabul());
+});
+
+gulp.task('test', function () {
+  return gulp.src(['test/unit/**', 'test/integration/**'])
+    .pipe(plugins.mocha())
+    .pipe(plugins.instabul.writeReports());
+});
