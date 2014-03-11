0ba98b52a14a33a04bb54e9978cd15c7686bf3a3
Ben Drucker <bvdrucker@gmail.com>
2014-03-11 11:55:04 -0400

    defer port to process if available

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 var Hapi = require('hapi');
 
-var server = new Hapi.Server('localhost', 8000, {cors: true});
+var server = new Hapi.Server('localhost', process.env.PORT || 8000, {cors: true});
 
 require('./routes/campaigns')(server);
 require('./routes/pledges')(server);
