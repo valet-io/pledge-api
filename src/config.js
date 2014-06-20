a2c52d67e36d76b3d61d9d498d0705443f2ccbb8
Ben Drucker <bvdrucker@gmail.com>
2014-06-20 08:17:22 -0400

    merge config changes

--- src/config.js
+++ src/config.js
@@ -1,12 +1,7 @@
 var nconf = require('nconf');
 
-<<<<<<< HEAD
 module.exports = nconf
   .env('_')
-=======
-nconf
-  .env('__')
->>>>>>> db55819... payment endpoint
   .defaults({
     database: {
       host: 'localhost',
