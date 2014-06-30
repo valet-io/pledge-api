7b763d604126363964ebd03c9184c597c6d14195
Ben Drucker <bvdrucker@gmail.com>
2014-06-30 17:37:49 -0400

    fix payment migration

--- migrations/20140630165554_payment.js
+++ migrations/20140630165554_payment.js
@@ -1,13 +1,13 @@
 'use strict';
 
 exports.up = function (knex, Promise) {
-  return knex.schema.table('campaigns', function (t) {
+  return knex.schema.table('pledges', function (t) {
     t.text('payment_id');
   });
 };
 
 exports.down = function (knex, Promise) {
-  return knex.schema.table('campaigns', function (t) {
+  return knex.schema.table('pledges', function (t) {
     t.dropColumn('payment_id');
   });
 };
