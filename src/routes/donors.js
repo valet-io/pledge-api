c05c45cf30e325d510a0fb72f07e578a045076db
Ben Drucker <bvdrucker@gmail.com>
2014-08-20 15:00:42 -0400

    Allow donors to be updated
    [Finishes #77302268]

--- src/routes/donors.js
+++ src/routes/donors.js
@@ -1,6 +1,8 @@
 'use strict';
 
 var Donor = require('../models/donor');
+var Joi   = require('joi');
+var _     = require('lodash');
 
 module.exports = function (server) {
 
@@ -18,6 +20,30 @@ module.exports = function (server) {
     config: {
       validate: {
         payload: Donor.prototype.schema
+      }
+    }
+  });
+
+  server.route({
+    method: 'PUT',
+    path: '/donors/{id}',
+    handler: function (request, reply) {
+      new Donor({id: request.params.id})
+        .save(_.omit(request.payload, 'id'), {
+          method: 'update',
+          require: true
+        })
+        .call('fetch', {require: true})
+        .done(reply, reply);
+    },
+    config: {
+      validate: {
+        params: {
+          id: Joi.string().guid().required()
+        },
+        payload: _.extend(_.clone(Donor.prototype.schema), {
+          id: Joi.valid(Joi.ref('$params.id'))
+        })
       }
     }
   });
--- test/integration/specs/donors.js
+++ test/integration/specs/donors.js
@@ -1,9 +1,12 @@
 'use strict';
 
 var expect = require('chai').expect;
+var uuid   = require('node-uuid');
 var server = require('../../server');
 
 describe('Donors', function () {
+
+  var donor = require('../seeds/donors')[0];
 
   describe('POST /donors', function () {
 
@@ -13,7 +16,7 @@ describe('Donors', function () {
         method: 'post',
         payload: JSON.stringify({
           name: 'Ben Drucker',
-          phone: '9739856070',
+          phone: '9739856070'
         })
       })
       .then(function (response) {
@@ -31,6 +34,71 @@ describe('Donors', function () {
       .then(function (response) {
         expect(response.statusCode).to.equal(400);
         expect(response.result.message).to.equal('name is required');
+      });
+    });
+
+  });
+
+  describe('/donors', function () {
+
+    it('updates the pledge by ID', function () {
+      return server.injectThen({
+        url: '/donors/' + donor.id,
+        method: 'put',
+        payload: JSON.stringify({
+          id: donor.id,
+          name: donor.name,
+          phone: donor.phone,
+          email: 'bvdrucker@gmail.com'
+        })
+      })
+      .then(function (response) {
+        expect(response.statusCode).to.equal(200);
+        expect(JSON.parse(response.payload)).to.have.property('email', 'bvdrucker@gmail.com');
+      });
+
+    });
+
+    it('responds with a 400 for non-uuid', function () {
+      return server.injectThen({
+        method: 'put',
+        url: '/donors/a'
+      })
+      .then(function (response) {
+        expect(response.statusCode).to.equal(400);
+      });
+    });
+
+    it('responds with a 404 if the donor does not exist', function () {
+      var id = uuid.v4();
+      return server.injectThen({
+        method: 'put',
+        url: '/donors/' + id,
+        payload: JSON.stringify({
+          id: id,
+          name: donor.name,
+          phone: donor.phone,
+          email: donor.email
+        })
+      })
+      .then(function (response) {
+        expect(response.statusCode).to.equal(404);
+      });
+    });
+
+    it('it ensures the url id matches the payload id', function () {
+      return server.injectThen({
+        method: 'put',
+        url: '/donors/' + donor.id,
+        payload: JSON.stringify({
+          id: uuid.v4(),
+          name: donor.name,
+          phone: donor.phone,
+          email: donor.email
+        })
+      })
+      .then(function (response) {
+        expect(response.statusCode).to.equal(400);
       });
     });
 
