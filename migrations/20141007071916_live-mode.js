2a3968c56851e99b0cd421c3f5862ce69046fb71
Ben Drucker <bvdrucker@gmail.com>
2014-10-24 10:04:29 -0400

    Update live mode migration to set all existing records to live

--- migrations/20141007071916_live-mode.js
+++ migrations/20141007071916_live-mode.js
@@ -6,6 +6,9 @@ exports.up = function (knex, Promise) {
   return Promise.map(tables, function (table) {
     return knex.schema.table(table, function (t) {
       t.boolean('live').defaultTo(true);
+    })
+    .then(function () {
+      return knex(table).update('live', true);
     });
   });
 };
