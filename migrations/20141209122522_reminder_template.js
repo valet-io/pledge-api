560308d0238967c8a762b87c7e6ed1b9d15e14c6
Ben Drucker <bvdrucker@gmail.com>
2014-12-09 13:04:07 -0500

    fix(campaign): fix template option on campaign

--- migrations/20141209122522_reminder_template.js
+++ migrations/20141209122522_reminder_template.js
@@ -1,13 +1,13 @@
 'use strict';
 
 exports.up = function (knex) {
-  knex.schema.table('campaigns', function (t) {
+  return knex.schema.table('campaigns', function (t) {
     t.text('reminder_template');
   });
 };
 
 exports.down = function (knex) {
-  knex.schema.table('campaigns', function (t) {
+  return knex.schema.table('campaigns', function (t) {
     t.dropColumn('reminder_template');
   });
 };
