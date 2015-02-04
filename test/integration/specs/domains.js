2a1e1bbb1241a1ad2cc1b45df211990915a094fc
Ben Drucker <bvdrucker@gmail.com>
2015-02-04 10:12:24 -0500

    Extract status assertion into common fn

--- test/integration/specs/domains.js
+++ test/integration/specs/domains.js
@@ -2,6 +2,7 @@
 
 var expect = require('chai').expect;
 var uuid   = require('node-uuid');
+var utils  = require('../utils')
 
 module.exports = function (server) {
 
@@ -14,33 +15,30 @@ module.exports = function (server) {
 
       it('can get a domain by id', function () {
         return server.injectThen('/domains/' + domain.id)
+          .tap(utils.assertStatus(200))
           .then(function (response) {
-            expect(response.statusCode).to.equal(200);
             expect(JSON.parse(response.payload)).to.have.property('name', 'ourgala.org');
           });
       });
 
       it('can get a domain by name', function () {
         return server.injectThen('/domains/ourgala.org')
+          .tap(utils.assertStatus(200))
           .then(function (response) {
-            expect(response.statusCode).to.equal(200);
             expect(JSON.parse(response.payload)).to.have.property('id', domain.id);
           });
       });
 
       it('can get the related campaign', function () {
         return server.injectThen('/domains/ourgala.org?expand[]=campaign')
+          .tap(utils.assertStatus(200))
           .then(function (response) {
-            expect(response.statusCode).to.equal(200);
             expect(JSON.parse(response.payload)).to.have.deep.property('campaign.id').with.length(36);
           });
       });
 
       it('responds with 404 if the domain is not found', function () {
-        return server.injectThen('/domains/theirgala.org')
-          .then(function (response) {
-            expect(response.statusCode).to.equal(404);
-          });
+        return server.injectThen('/domains/theirgala.org').then(utils.assertStatus(404));
       });
 
     });
